53603f0c9e4d0c40a46a09b784081f310c3f7fd5
Adnan Arodiya <aiarodiya07@gmail.com>
2024-01-04 18:03:32 +0530

    Update About component UI and content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,25 +1,28 @@
 export default function About() {
   return (
-    <div className="flex justify-center items-center gap-8 h-[80vh]">
+    <div className="flex flex-col md:flex-row justify-center items-center gap-8 h-[80vh]">
       <div>
         <img
-          className="w-96 rounded-3xl hover:scale-95 hover:duration-500 duration-700"
+          className="w-32 md:w-80 xl:w-96 rounded-3xl hover:scale-95 hover:duration-500 duration-700"
           src="img/profile.jpg"
           alt=""
         />
       </div>
       <div>
-        <h1 className="text-3xl mb-4">Hi 👋! My name is Adnan</h1>
-        <h3 className="text-xl mb-4">Web Developer | Student | Learner</h3>
-        <p>
-          🌱 I’m currently learning React <br />
-          📫 How to reach me aiarodiya07@gmail.com
-          <br /> 👨‍💻 All of my projects are available at Github <br />
-          💬 Ask me about Html,Css,C,JavaScript,tailwind,Sass,React,TypeScript.
-          <br /> 🌍 I'm based in India
-          <br /> 🖥️ See my portfolio at My website <br />
-          ✉️ You can contact me at aiarodiya07@gmail.com
-        </p>
+        <h1 className="font-medium xl:text-3xl mb-4">
+          Hi 👋! My name is Adnan
+        </h1>
+        <h3 className="italic xl:text-xl mb-4">
+          Web Developer | Student | Learner
+        </h3>
+        <div className="text-base w-60 md:w-80 xl:w-96">
+          I'm a computer engineering student deeply passionate about web
+          development. Specializing in front-end technologies like HTML, CSS,
+          SaaS, Tailwind, React, JavaScript, Python, C, Bootstrap, and Figmac, I
+          bring a versatile skill set to the table. Beyond coding, I find joy in
+          playing video games during my free time, creating a perfect balance
+          between my technical skills and leisure activities.
+        </div>
       </div>
     </div>
   );
